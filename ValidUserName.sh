fb3b9d5572aa241657ac0bc4ef4e5d8d6b000bb2
umesh <usp5758@gmail.com>
2020-05-11 22:06:50 +0530

    Valid first name

--- ValidUserName.sh
+++ ValidUserName.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 echo "Welcome to user Registration"
+
+read -p "Enter first name" firstName
+firstNamePattern="^[[:upper:]]{1}[[:lower:]]{3,}$"
+if [[ $firstName =~ $FirstNamePattern ]]
+then
+        echo "Valid FirstName"
+else
+        echo "Invalid FirstName"
+fi
