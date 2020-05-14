926db52c440d7c548f3b00af36d311f1838b26c2
umesh <usp5758@gmail.com>
2020-05-14 11:06:23 +0530

    add 1 numeric number

--- password.sh
+++ password.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
-read -p "Enter password: " passRule2
-passPattern2="(?=[^A-Z]*[A-Z])[[:alnum:]]{8,}$"
-if [[ $passRule2 =~ $passPattern2 ]]
+read -p "Enter password: " passRule3
+passPattern3="^((?=.*([0-9])(?=.*[A-Z])[[:alnum:]]{8,}$"
+if [[ $passRule3 =~ $passPattern3 ]]
 then
         echo "Password Validated"
 else
